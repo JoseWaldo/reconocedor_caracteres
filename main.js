bffe3c713765de4ac04f350a46f484580a03ea93
Jose Waldo <josferw@gmail.com>
2023-05-27 19:19:24 -0500

    Feat: añadi la logica del canvas para dibujar sobre el y obtener la imagen

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const btnCleanCanvas = document.getElementById("btnCleanCanvas");
+const btnIdentifyImg = document.getElementById("btnIdentifyImg");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
+const CANVAS_COLOR = "#00ff00";
 
-setupCounter(document.querySelector('#counter'))
+let initialX;
+let initialY;
+
+ctx.fillStyle = CANVAS_COLOR;
+ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+const dibujar = (cursorX, cursorY) => {
+  ctx.beginPath();
+  ctx.moveTo(initialX, initialY);
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = "#000";
+  ctx.lineCap = "round";
+  ctx.lineJoin = "round";
+  ctx.lineTo(cursorX, cursorY);
+  ctx.stroke();
+
+  initialX = cursorX;
+  initialY = cursorY;
+};
+
+const mouseMoving = (e) => {
+  dibujar(e.offsetX, e.offsetY);
+};
+
+const mouseDown = (e) => {
+  initialX = e.offsetX;
+  initialY = e.offsetY;
+  dibujar(initialX, initialY);
+  canvas.addEventListener("mousemove", mouseMoving);
+};
+
+const mouseUp = (e) => {
+  canvas.removeEventListener("mousemove", mouseMoving);
+};
+
+const cleanCanvas = (e) => {
+  e.preventDefault();
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillStyle = CANVAS_COLOR;
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
+
+// const downloadCanvas = (e) => {
+//   e.preventDefault();
+//   const link = document.createElement("a");
+//   link.href = canvas.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
+//   link.download = "canvas_image.jpg"; // Nombre del archivo de descarga
+//   link.click();
+// };
+
+const downloadCanvas = () => {
+  const img = document.createElement("img");
+  img.src = canvas.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
+
+  document.body.appendChild(img);
+};
+
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("mouseup", mouseUp);
+btnCleanCanvas.addEventListener("click", cleanCanvas);
+btnIdentifyImg.addEventListener("click", downloadCanvas);
