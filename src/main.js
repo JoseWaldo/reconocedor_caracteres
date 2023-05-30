0b8614dcd97a2d3c19077034597dbc1119934050
Jose Waldo <josferw@gmail.com>
2023-05-29 20:42:48 -0500

    Commit para enviar al repo remoto

--- src/main.js
+++ src/main.js
@@ -26,6 +26,13 @@ class L2 {
 }
 tf.serialization.registerClass(L2);
 
+let modelo;
+(async () => {
+  console.log("Cargando modelo...");
+  modelo = await tf.loadLayersModel("./iaModel/model.json");
+  console.log("Modelo cargado...");
+})();
+
 const dibujar = (cursorX, cursorY) => {
   ctx.beginPath();
   ctx.moveTo(initialX, initialY);
@@ -70,23 +77,58 @@ const cleanCanvas = (e) => {
 //   link.click();
 // };
 
-const downloadCanvas = () => {
+const downloadCanvas = async () => {
   // const img = document.createElement("img");
   // img.src = canvas.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
 
   // document.body.appendChild(img);
   resample_single(canvas, 128, 128, canvasSmall);
-  let imgData = ctx2.getImageData(0, 0, 128, 128);
+
+  // const link = document.createElement("a");
+  // link.href = canvasSmall.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
+  // link.download = "canvas_image.jpg"; // Nombre del archivo de descarga
+  // link.click();
+
+  // let imgData = ctx2.getImageData(0, 0, 128, 128);
+  // const pixelData = imgData.data;
+
+  // console.log(pixelData);
+  // const tensor = tf.tensor3d(pixelData, [128, 128, 1], "int32");
+  // const prediction = await modelo.predict(tensor);
+  // const result = prediction.dataSync();
+
+  // console.log("Prediccion: ", result);
+
+  var imgData = ctx2.getImageData(0, 0, 128, 128);
+  var arr = [];
+  var arr128 = [];
+
+  for (var p = 0, i = 0; p < imgData.data.length; p += 4) {
+    var r = imgData.data[p];
+    var g = imgData.data[p + 1];
+    var b = imgData.data[p + 2];
+
+    var valor = [r / 255, g / 255, b / 255]; // Normalizar los valores de 0-255 a 0-1
+
+    arr128.push(valor);
+
+    if (arr128.length == 128) {
+      arr.push(arr128);
+      arr128 = [];
+    }
+  }
+
+  arr = [arr];
+
+  var tensor4 = tf.tensor4d(arr);
+  var resultados = modelo.predict(tensor4).dataSync();
+  var mayorIndice = resultados.indexOf(Math.max.apply(null, resultados));
+
+  console.log(resultados);
+  console.log("Predicción:", mayorIndice);
 };
 
 canvas.addEventListener("mousedown", mouseDown);
 canvas.addEventListener("mouseup", mouseUp);
 btnCleanCanvas.addEventListener("click", cleanCanvas);
 btnIdentifyImg.addEventListener("click", downloadCanvas);
-
-let modelo;
-(async () => {
-  console.log("Cargando modelo...");
-  modelo = await tf.loadLayersModel("./iaModel/model.json");
-  console.log("Modelo cargado...");
-})();
