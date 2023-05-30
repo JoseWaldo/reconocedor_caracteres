8c65db7b8070d52fe42aafbc222b1ca87c02a28b
Jose Waldo <josferw@gmail.com>
2023-05-29 21:55:07 -0500

    Ultimos cambios hechos

--- src/main.js
+++ src/main.js
@@ -11,6 +11,71 @@ const CANVAS_WIDTH = canvas.width;
 const CANVAS_HEIGHT = canvas.height;
 const CANVAS_COLOR = "#fff";
 
+const CLASS_NAMES = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "_A",
+  "_B",
+  "_C",
+  "_D",
+  "_E",
+  "_F",
+  "_G",
+  "_H",
+  "_I",
+  "_J",
+  "_K",
+  "_L",
+  "_M",
+  "_N",
+  "_O",
+  "_P",
+  "_Q",
+  "_R",
+  "_S",
+  "_T",
+  "_U",
+  "_V",
+  "_W",
+  "_X",
+  "_Y",
+  "_Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 let initialX;
 let initialY;
 
@@ -69,63 +134,13 @@ const cleanCanvas = (e) => {
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 };
 
-// const downloadCanvas = (e) => {
-//   e.preventDefault();
-//   const link = document.createElement("a");
-//   link.href = canvas.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
-//   link.download = "canvas_image.jpg"; // Nombre del archivo de descarga
-//   link.click();
-// };
-
-const downloadCanvas = async () => {
-  // const img = document.createElement("img");
-  // img.src = canvas.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
-
-  // document.body.appendChild(img);
+const downloadCanvas = (e) => {
+  e.preventDefault();
   resample_single(canvas, 128, 128, canvasSmall);
-
-  // const link = document.createElement("a");
-  // link.href = canvasSmall.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
-  // link.download = "canvas_image.jpg"; // Nombre del archivo de descarga
-  // link.click();
-
-  // let imgData = ctx2.getImageData(0, 0, 128, 128);
-  // const pixelData = imgData.data;
-
-  // console.log(pixelData);
-  // const tensor = tf.tensor3d(pixelData, [128, 128, 1], "int32");
-  // const prediction = await modelo.predict(tensor);
-  // const result = prediction.dataSync();
-
-  // console.log("Prediccion: ", result);
-
-  var imgData = ctx2.getImageData(0, 0, 128, 128);
-  var arr = [];
-  var arr128 = [];
-
-  for (var p = 0, i = 0; p < imgData.data.length; p += 4) {
-    var r = imgData.data[p];
-    var g = imgData.data[p + 1];
-    var b = imgData.data[p + 2];
-
-    var valor = [r / 255, g / 255, b / 255]; // Normalizar los valores de 0-255 a 0-1
-
-    arr128.push(valor);
-
-    if (arr128.length == 128) {
-      arr.push(arr128);
-      arr128 = [];
-    }
-  }
-
-  arr = [arr];
-
-  var tensor4 = tf.tensor4d(arr);
-  var resultados = modelo.predict(tensor4).dataSync();
-  var mayorIndice = resultados.indexOf(Math.max.apply(null, resultados));
-
-  console.log(resultados);
-  console.log("Predicción:", mayorIndice);
+  const link = document.createElement("a");
+  link.href = canvasSmall.toDataURL(); // Obtener la imagen en formato base64 desde el canvas
+  link.download = "canvas_image.jpg"; // Nombre del archivo de descarga
+  link.click();
 };
 
 canvas.addEventListener("mousedown", mouseDown);
